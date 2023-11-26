81f3e2db6af0d2c406f2c159d3f5b991dda35de5
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-11-26 04:41:42 +0100

    feat: add middleware to check that request is JSON
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

---
+++ tests/Feature/Http/Middleware/JsonOnlyMiddlewareTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Http\Middleware;
+
+use Tests\TestCase;
+
+class JsonOnlyMiddlewareTest extends TestCase
+{
+    /**
+     * Test that non-JSON requests are rejected.
+     */
+    public function test_non_json_requests_are_rejected(): void
+    {
+        $response = $this->post('/', [], ['Accept' => 'application/xml']);
+
+        $response->assertStatus(400)
+            ->assertJson([
+                'message' => 'Only JSON requests are accepted'
+            ]);
+    }
+
+    /**
+     * Test that JSON requests are accepted.
+     */
+    public function test_json_requests_are_accepted(): void
+    {
+        $response = $this->post('/', [], ['Accept' => 'application/json']);
+
+        $response->assertStatus(200);
+    }
+}
