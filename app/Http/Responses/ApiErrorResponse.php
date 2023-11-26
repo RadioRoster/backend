d3a7473243cac7e34ea4b3a103dccb50cb695f7e
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-11-26 05:03:31 +0100

    feat: add default response consturct
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

---
+++ app/Http/Responses/ApiErrorResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\Response;
+use Throwable;
+
+class ApiErrorResponse implements Responsable
+{
+    /**
+     * Class representing an API error response.
+     *
+     * This class is responsible for constructing an API error response object
+     * with the provided message, optional exception, status code, and headers.
+     */
+    public function __construct(
+        private $message,
+        private ?Throwable $exception = null,
+        private int $status = Response::HTTP_INTERNAL_SERVER_ERROR,
+        private array $headers = []
+    ) {}
+
+    /**
+     * Convert the exception to a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        $response = ['message' => $this->message];
+
+        if (! is_null($this->exception) && config('app.debug')) {
+            $response['debug'] = [
+                'message'   => $this->exception->getMessage(),
+                'file'      => $this->exception->getFile(),
+                'line'      => $this->exception->getLine(),
+                'trace'     => $this->exception->getTrace(),
+            ];
+        }
+
+        return response()->json([
+            $response,
+            'status' => $this->status,
+            'timestamp' => now()->toDateTimeString(),
+        ], $this->status, $this->headers);
+    }
+
+}
---
+++ app/Http/Responses/ApiSuccessResponse.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\Response;
+
+class ApiSuccessResponse implements Responsable
+{
+    /**
+     * Class representing a successful API response.
+     *
+     * This class is responsible for constructing a successful API response
+     * with the provided data, status code, and headers.
+     */
+    public function __construct(
+        private $data,
+        private int $status = Response::HTTP_OK,
+        private array $headers = []
+    ) {}
+
+    /**
+     * Convert the response instance to a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response|void
+     */
+    public function toResponse($request)
+    {
+        return response()->json(
+            [
+                'status' => $this->status,
+                'data' => $this->data,
+                'timestamp' => now()->toDateTimeString(),
+            ],
+            $this->status,
+            $this->headers
+        );
+    }
+
+}
