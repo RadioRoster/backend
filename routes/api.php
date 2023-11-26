8891ca4d564c8b9e4d38efcda2d47719046f45aa
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-11-26 04:14:25 +0100

    feat: add login
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * Defines the API routes for version 1.
+ *
+ * @group v1
+ */
+Route::group(['prefix' => 'v1'], function () {
+    require __DIR__ . '/api/v1/auth.php';
 });
