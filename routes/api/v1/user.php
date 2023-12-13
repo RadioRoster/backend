5e7c6576bfcf211a500ca69431d7b7446e3082ec
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-12-13 04:55:23 +0100

    update routes to simplify identification
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/users', [UserController::class, 'index'])->name('api.v1.users.index');
     Route::post('/users', [UserController::class, 'store'])->name('api.v1.users.store');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('api.v1.users.show');
-    Route::patch('/users/{id}', [UserController::class, 'update'])->name('api.v1.users.update');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('api.v1.users.destroy');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('api.v1.users.show');
+    Route::patch('/users/{user}', [UserController::class, 'update'])->name('api.v1.users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('api.v1.users.destroy');
 });
