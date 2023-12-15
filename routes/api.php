527fb4adf4ac2dde11d79279553baa4704b5a847
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-12-15 01:00:44 +0100

    add role routes
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     require __DIR__ . '/api/v1/auth.php';
     require __DIR__ . '/api/v1/user.php';
+    require __DIR__ . '/api/v1/role.php';
 });
