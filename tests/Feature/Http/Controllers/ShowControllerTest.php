e68df6f7765ee83edb7c24c8021be46fdb44abe2
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-12-29 19:38:18 +0100

    Add generated  test
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- tests/Feature/Http/Controllers/ShowControllerTest.php
+++ tests/Feature/Http/Controllers/ShowControllerTest.php
@@ -2,19 +2,52 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Show;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ShowControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * Test retrieving a paginated list of shows.
+     *
+     * @return void
      */
-    public function test_example(): void
+    public function testIndex(): void
     {
+        // Create some test data
+        $shows = Show::factory()->count(5)->create();
+
+        // Make a GET request to the index endpoint
         $response = $this->get('/api/v1/shows');
 
+        // Assert that the response has a successful status code
         $response->assertStatus(200);
+
+        // Assert that the response contains the correct number of shows
+        $response->assertJsonCount(5, 'data');
+
+        // Assert that the response contains the correct show data
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'start_date',
+                    'end_date',
+                    'live',
+                    'enabled',
+                    'moderators' => [
+                        '*' => [
+                            'id',
+                            'name',
+                            'primary',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
     }
 }
