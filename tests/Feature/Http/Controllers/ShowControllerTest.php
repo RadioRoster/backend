b1bb8f0a04cf8ba5b5210ce331f5c761e0d312ad
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2024-02-10 01:41:31 +0100

    Add test for retrieving paginated list of shows
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- tests/Feature/Http/Controllers/ShowControllerTest.php
+++ tests/Feature/Http/Controllers/ShowControllerTest.php
@@ -3,9 +3,14 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Show;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
+/**
+ * @coversDefaultClass \App\Http\Controllers\ShowController
+ */
 class ShowControllerTest extends TestCase
 {
     use RefreshDatabase;
@@ -13,21 +18,31 @@ class ShowControllerTest extends TestCase
     /**
      * Test retrieving a paginated list of shows.
      *
+     * @covers ::index
      * @return void
      */
-    public function testIndex(): void
+    public function test_index(): void
     {
+        // Create some test user
+        User::factory()->count(30)->create();
+
         // Create some test data
-        $shows = Show::factory()->count(5)->create();
+        $shows = Show::factory([
+            'enabled' => true,
+        ])->withUser()->count(10)->create();
+
+        $today = today();
+        $inAMonth = today()->addMonth();
+        $perPage = 5;
 
         // Make a GET request to the index endpoint
-        $response = $this->get('/api/v1/shows');
+        $response = $this->getJson('/api/v1/shows?start_date=' . $today . '&end_date=' . $inAMonth . '&per_page=' . $perPage);
 
         // Assert that the response has a successful status code
         $response->assertStatus(200);
 
         // Assert that the response contains the correct number of shows
-        $response->assertJsonCount(5, 'data');
+        $response->assertJsonCount($perPage, 'data');
 
         // Assert that the response contains the correct show data
         $response->assertJsonStructure([
@@ -37,7 +52,7 @@ class ShowControllerTest extends TestCase
                     'title',
                     'start_date',
                     'end_date',
-                    'live',
+                    'is_live',
                     'enabled',
                     'moderators' => [
                         '*' => [
@@ -48,6 +63,19 @@ class ShowControllerTest extends TestCase
                     ],
                 ],
             ],
+        ]);
+
+        // Sort the shows by start date and take the first 5
+        $shows = $shows->sortBy('start_date')->values()->take($perPage);
+
+        // Assert that the response contains the correct show data
+        $response->assertJsonFragment([
+            'id' => $shows->first()->id,
+            'title' => $shows->first()->title,
+            'start_date' => $shows->first()->start_date,
+            'end_date' => $shows->first()->end_date,
+            'is_live' => $shows->first()->is_live,
+            'enabled' => $shows->first()->enabled,
         ]);
     }
 }
